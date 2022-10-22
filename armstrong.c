a07a8e126db8144b3271c35d44dec4b684d6431b
Santosh pant <72144199+Santoshpant23@users.noreply.github.com>
2022-10-22 12:13:19 +0545

    Added Armstrong Program

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c = 0, d, e;
+    printf("Enter your number\n");
+    scanf("%d", &a);
+    e = a;
+    while (a != 0)
+    {
+        b = a % 10;
+        d = b * b * b;
+        c = c + d;
+        a = a / 10;
+    }
+
+    if (e == c)
+    {
+        printf("The number is armstrong\n");
+    }
+    else
+    {
+        printf("The number is not armstrong");
+    }
+
+    return 0;
+}
