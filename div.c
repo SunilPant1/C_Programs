6e23fba1a4bf8f2fe23cd4f6751e081dacf809ac
samirsaud <116507026+samirsaud@users.noreply.github.com>
2022-10-23 22:04:16 +0545

    Add files via upload

---
+++ div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,sum;
+    printf("enter the numbeus:");
+    scanf("%d%d",&x,&y);
+
+    sum=x/y;
+    printf("%d is the sum of the numbers",sum);
+    return 0;
+
+    }    
