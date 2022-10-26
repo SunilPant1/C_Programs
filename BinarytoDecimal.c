e5bedaa75f06dfdd29ebac9b37099fa7a1bdea27
Akash Barman <92024930+itstheakash@users.noreply.github.com>
2022-10-27 00:28:59 +0530

    Create BinarytoDecimal.c

---
+++ BinarytoDecimal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+// Binary number to Decimal Number
+
+int main()
+{
+  int rem, i = 0, dec = 0;
+  long long n;
+  printf("Enter any Binary Number: ");
+  scanf("%lld", &n);
+
+  while (n!=0) 
+  {
+    rem = n % 10;
+    n = n / 10;
+    dec = dec + rem * pow(2, i);
+    ++i;
+  }
+
+  printf("Conversion to Decimal Number: %d", dec);
+
+  return 0;
+}
