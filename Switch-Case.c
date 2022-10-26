f7d867c0c9eb960ca647a4e4602fb7254d31aa50
Sunilpant111 <116721013+Sunilpant111@users.noreply.github.com>
2022-10-26 19:21:46 +0545

    switch case program added

---
+++ Switch-Case.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    printf("Enter your preferred number\n");
+    int age ;
+    scanf("%d" , &age);
+    switch (age)
+    {
+    case 18:
+        printf("You are mature enough");
+        break;
+
+     case 17 :
+        printf("Just one year more");
+        break;
+
+    case 19:
+        printf("You are perfectly ready");
+        break;
+
+    
+    default:
+        printf("Hey bro program has ended");
+      
+    }
+    return 0;
+}
